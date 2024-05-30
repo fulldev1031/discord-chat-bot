2c03297187bed09cd7b6d85039eaa9c6679ddb9f
fulldev1031 <fulldev1031@gmail.com>
2024-05-30 15:49:13 +0800

    chore: art provider updated

--- src/bot.py
+++ src/bot.py
@@ -153,8 +153,7 @@ https://github.com/fulldev1031/discord-chat-bot""")
     @app_commands.choices(model=[
         app_commands.Choice(name="gemini", value="gemini"),
         app_commands.Choice(name="openai", value="openai"),
-        app_commands.Choice(name="bing", value="bing"),
-        app_commands.Choice(name="you", value="You"),
+        app_commands.Choice(name="bing", value="BingCreateImages"),
     ])
     async def draw(interaction: discord.Interaction, *, prompt: str, model: app_commands.Choice[str]):
         if interaction.user == discordClient.user:
